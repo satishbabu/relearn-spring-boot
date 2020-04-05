53238334710ba010a5ab4d4e4b0aeb4c31ca0127
satishbabu <reachsatish@yahoo.com>
2020-04-04 21:19:48 -0400

    Added save

--- src/main/java/com/apnatriangle/relearnspringboot/user/UserController.java
+++ src/main/java/com/apnatriangle/relearnspringboot/user/UserController.java
@@ -2,9 +2,7 @@ package com.apnatriangle.relearnspringboot.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,11 @@ public class UserController {
     @GetMapping ("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
     }
 
 }
