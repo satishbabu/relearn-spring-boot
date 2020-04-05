0db582a088bfe39fc03f81b9560b9c4929af503f
satishbabu <reachsatish@yahoo.com>
2020-04-05 03:53:18 -0400

    Return current HTTPStatus for createUser (uri to saved user) and retrieveUser (User Not Found)

--- src/main/java/com/apnatriangle/relearnspringboot/user/UserController.java
+++ src/main/java/com/apnatriangle/relearnspringboot/user/UserController.java
@@ -2,8 +2,11 @@ package com.apnatriangle.relearnspringboot.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,12 +21,22 @@ public class UserController {
 
     @GetMapping ("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        final User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+
     }
 
 }
